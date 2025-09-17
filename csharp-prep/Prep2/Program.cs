15ab0727c9bd1d0d080a8f441189bb28697dfbec
SilasCarlson <silascarlson@byui.edu>
2025-09-17 09:47:51 -0600

    W01 Prove Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+
+        int gradePercentage = int.Parse(userInput);
+
+        // Get the letter grade of the user
+        string gradeLetter = "F";
+        if (gradePercentage >= 90)
+        {
+            gradeLetter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            gradeLetter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            gradeLetter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            gradeLetter = "D";
+        }
+
+        // Stretch Challenge
+        string gradeLetterSign = "";
+        if (gradePercentage % 10 >= 7)
+        {
+            gradeLetterSign = "+";
+        }
+        else if (gradePercentage % 10 < 3)
+        {
+            gradeLetterSign = "-";
+        }
+
+        if (gradeLetter == "F") gradeLetterSign = "";
+        if (gradeLetter == "A" && gradeLetterSign == "+") gradeLetterSign = "";
+
+        Console.WriteLine($"Your letter grade is: {gradeLetter}{gradeLetterSign}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed the class, congragulations!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass the class, better luck next time!");
+        }
     }
 }
