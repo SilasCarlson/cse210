d7e86f4602f3858ba6ed689e1d64510a07ab0c87
SilasCarlson <silascarlson@byui.edu>
2025-10-13 09:14:47 -0600

    W05 Prepare

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(6);
+        Fraction fraction3 = new Fraction(6, 7);
+
+        fraction1.SetTop(2);
+        fraction2.SetBottom(6);
+
+        Console.WriteLine($"Fraction 1: {fraction1.GetFractionString()} ({fraction1.GetDecimalValue()})");
+        Console.WriteLine($"Fraction 2: {fraction2.GetFractionString()} ({fraction2.GetDecimalValue()})");
+        Console.WriteLine($"Fraction 3: {fraction3.GetFractionString()} ({fraction3.GetDecimalValue()})");
     }
 }
