8e77a5ae9d50efb43ac9bcff72b5586de25d092a
Paughton <113547668+SilasCarlson@users.noreply.github.com>
2025-10-29 12:16:36 -0600

    W07 Prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Assignment (general)
+        Assignment assignment1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment1.GetSummary());
+
+        // Math assignment
+        MathAssignment assignment2 = new MathAssignment("Samuel Bennett", "Fractions", "7.3", "Problems 8-19");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        //Writing Assignment
+        WritingAssignment assignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
