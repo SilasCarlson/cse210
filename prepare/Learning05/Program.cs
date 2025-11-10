7b429834b93f6bb8850f787f0121657fb0f40a56
Paughton <113547668+SilasCarlson@users.noreply.github.com>
2025-11-10 09:45:44 -0700

    W09 Prepare

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = [
+            new Square("Green", 5),
+            new Rectangle("Blue", 20, 5),
+            new Circle("Red", 5)
+        ];
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
